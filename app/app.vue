e613f909ae7b04c11125d31c82c1c9e1e3cb9ef9
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-07-24 10:58:43 -0500

    chore: Update SEO improvements

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,24 @@
+import { NuxtLoadingIndicator } from '../.nuxt/components';
+<script setup lang="ts">
+useHead({
+  title: 'Teshane Crawford - Software Engineer',
+  meta: [
+    {
+      name: 'description',
+      content: 'Teshane Crawford is a software engineer based in New York City.'
+    }
+  ]
+})
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtRouteAnnouncer>
+      <template #default="{ message }">
+        <p>{{ message }} was loaded.</p>
+      </template>
+    </NuxtRouteAnnouncer>
+    <NuxtLoadingIndicator />
+    <NuxtPage />
   </div>
 </template>
---
+++ app/pages/index.vue
@@ -0,0 +1,14 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+const props = defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div>
+    <h1>{{ props.msg }}</h1>
+  </div>
+</template>
+
+<style scoped></style>
