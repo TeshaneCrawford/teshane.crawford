4351fa12566a5cf86e3820e0aeeff4fd828da074
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-07-25 13:12:58 -0500

    chore: Update uno.config.ts with new presets and theme colors

--- uno.config.ts
+++ uno.config.ts
@@ -1,38 +1,59 @@
 import {
-    defineConfig,
-    presetAttributify,
-    presetIcons,
-    presetTypography,
-    presetUno,
-    presetWebFonts,
-    transformerDirectives,
-    transformerVariantGroup
-  } from 'unocss'
-  
-  export default defineConfig({
-    shortcuts: [
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetUno,
+  presetWebFonts,
+  transformerDirectives,
+  transformerVariantGroup,
+} from 'unocss'
+
+export default defineConfig({
+  shortcuts: [
+    // ...
+  ],
+  theme: {
+    colors: {
       // ...
-    ],
-    theme: {
-      colors: {
-        // ...
-      }
     },
-    presets: [
-      presetUno(),
-      presetAttributify(),
-      presetIcons(),
-      presetTypography(),
-      presetWebFonts({
-        fonts: {
-            sans: 'DM Sans, sans-serif',
-            mono: 'DM Mono, monospace',
-            dank: 'Dank Mono, monospace',
+  },
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons({
+      extraProperties: {
+        'display': 'inline-block',
+        'height': '1.2em',
+        'width': '1.2em',
+        'vertical-align': 'text-bottom',
+      },
+    }),
+    presetTypography({
+      cssExtend: {
+        a: {
+          // eslint-disable-next-line @stylistic/quote-props
+          color: 'inherit',
+          'font-weigth': '700',
+          'text-decoration': 'none',
         },
-      }),
-    ],
-    transformers: [
-      transformerDirectives(),
-      transformerVariantGroup(),
-    ],
-  })
+      },
+    }),
+    presetWebFonts({
+      fonts: {
+        sans: 'DM Sans:400, 500, 600, 700',
+        mono: 'DM Mono',
+        // dank: 'Dank Mono, monospace',
+      },
+    }),
+  ],
+  transformers: [
+    transformerDirectives(),
+    transformerVariantGroup(),
+  ],
+  safelist: [
+    'i-ri-menu-2-fill',
+    Array.from({ length: 5 }, (j, i) => `fill-level-${i}`),
+    'sm-fsc max-w-75'.split(' '),
+  ].flat(),
+})
