2ac5a78f81ff6c7ac9e25e8ad35be94e8feb8d75
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-07-25 13:14:04 -0500

    feat: Add BgNoise component for displaying a background noise effect

---
+++ app/components/app/BgNoise.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <svg
+    class="pointer-events-none fixed isolate z-10 opacity-90 mix-blend-soft-light h-100dvh w-full"
+  >
+    <filter id="pedroduarteisalegend">
+      <feTurbulence
+        type="fractalNoise"
+        baseFrequency="0.80"
+        numOctaves="4"
+        stitchTiles="stitch"
+      />
+    </filter>
+    <rect
+      width="100%"
+      height="100%"
+      filter="url(#pedroduarteisalegend)"
+    />
+  </svg>
+  <div class="fixed w-full h-24 bg-gray-1/80 blur-[150px]" />
+</template>
+
+<style scoped></style>
