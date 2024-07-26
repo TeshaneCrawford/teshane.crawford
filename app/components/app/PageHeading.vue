8a7a163ed328e7c0b379fbb787849dc9b57e20ff
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-07-26 18:13:26 -0500

    feat: Add PageHeading component for displaying the page title and description

---
+++ app/components/app/PageHeading.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+defineProps<{
+  title?: string
+  description?: string
+}>()
+</script>
+
+<template>
+  <div class="sm:px-8 lg:px-16">
+    <div class="max-w-screen-xl mx-auto px-4 sm:px-8 lg:px-12">
+      <div class="max-w-2xl lg:max-w-5xl mx-auto flex flex-col gap-16">
+        <div
+          v-if="title || description || $slots.title
+            || $slots.description"
+          class="flex flex-col gap-4"
+        >
+          <AppPageTitle>
+            <slot name="title">
+              {{ title }}
+            </slot>
+          </AppPageTitle>
+          <p class="max-w-4xl text-gray-5 dark:text-gray-4 font-mono text-sm sm:text-base">
+            <slot name="description">
+              {{ description }}
+            </slot>
+          </p>
+        </div>
+
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ app/components/app/PageTitle.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  text?: string
+}>()
+</script>
+
+<template>
+  <h1 class="text-4xl sm:text-5xl font-bold tracking-tight text-gray-8 dark:text-gray-1">
+    <slot>{{ text }}</slot>
+  </h1>
+</template>
+
+<style scoped></style>
