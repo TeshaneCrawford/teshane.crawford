0c26055581ab8cd5e927021b79b743236be0120a
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-07-26 22:56:08 -0500

    refactor: Update project.ts with types for Project and ProjectList

---
+++ types/project.ts
@@ -0,0 +1,22 @@
+import type { Endpoints } from '@octokit/types'
+import type { ParsedContent } from '@nuxt/content'
+
+// export interface JsonParsedContent<T> extends ParsedContent {
+//   body: T
+// }
+export interface Project {
+  name: string
+  description: string
+  icon?: string
+  webLink: string
+  githubUrl?: string
+  tags: { name: string, icon: string }[]
+}
+
+export interface ProjectList extends ParsedContent {
+  name: string
+  projects: Project[]
+}
+
+export type Repo = Endpoints['GET /user/repos']['response']['data'][number]
+export type User = Endpoints['GET /user']['response']['data']
