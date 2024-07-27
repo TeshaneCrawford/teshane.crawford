f614cfe0bd09e06f96b3d6c707d141c95f515226
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-07-26 22:56:21 -0500

    feat: Add projects page with dynamic content loading

---
+++ app/pages/projects.server.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const route = useRoute()
+const { data: doc } = await useAsyncData(route.path, () => queryContent(route.path).findOne())
+</script>
+
+<template>
+  <AppPageHeading
+    :title="doc?.title"
+    :description="doc?.description"
+  >
+    <ProjectSection />
+  </AppPageHeading>
+</template>
+
+<style scoped></style>
