e6110e12f54c755061978e32e23a6bf3af0b50ba
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-07-27 14:36:34 -0500

    feat: Add HomeCard and Card components for displaying blog articles

---
+++ app/components/blog/HomeCard.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+const { error } = await useAsyncData('blogs', () => queryContent('/blogs/').findOne())
+
+if (error.value) {
+  throw createError({
+    statusCode: 404,
+    message: error.value.message || 'Page not found',
+    fatal: true,
+  })
+}
+
+const {
+  fetchBlogArticles,
+  articles,
+} = useBlog()
+
+await fetchBlogArticles()
+
+defineProps<{
+  blogtitle?: string
+}>()
+</script>
+
+<template>
+  <div
+    v-for="item in articles"
+    :key="item._path"
+  >
+    <BlogCard
+      :path="item._path!"
+      :title="item.title"
+      :description="item.description"
+      :published-at="item.publishedAt"
+      :authors="item.authors"
+    />
+  </div>
+</template>
+
+<style scoped></style>
