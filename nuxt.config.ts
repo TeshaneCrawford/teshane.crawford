83ff900bec3b537d984b5fd7d10ae355cf5a2134
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-08-01 09:06:21 -0500

    refactor: Update font styling and add Google Fonts for improved typography

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,6 +27,23 @@ export default defineNuxtConfig({
     '@unocss/reset/tailwind.css',
   ],
 
+  app: {
+    head: {
+      title: 'Teshane Crawford',
+      meta: [{ charset: 'utf-8' }],
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap',
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&display=swap',
+        },
+      ],
+    },
+  },
+
   // https://eslint.nuxt.com
   eslint: {
     config: {
