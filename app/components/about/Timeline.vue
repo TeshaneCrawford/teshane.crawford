2accca26a194c7cdaa2c29e6d85bb90ce09bab60
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-07-31 23:52:27 -0500

    refactor: Update Timeline.vue component with employment history details

---
+++ app/components/about/Timeline.vue
@@ -0,0 +1,121 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <!-- Item -->
+    <div class="group relative flex gap-x-5">
+      <!-- Icon -->
+      <div class="relative group-last:after:hidden after:absolute after:top-8 after:bottom-2 after:start-3 after:w-px after:-translate-x-[0.5px] after:bg-gray-200 dark:after:bg-neutral-700">
+        <div class="relative z-10 size-6 flex justify-center items-center">
+          <svg
+            class="shrink-0 size-6 text-gray-800 dark:text-neutral-200"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ><path d="M12 12h.01" /><path d="M16 6V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v2" /><path d="M22 13a18.15 18.15 0 0 1-20 0" /><rect
+            width="20"
+            height="14"
+            x="2"
+            y="6"
+            rx="2"
+          /></svg>
+        </div>
+      </div>
+      <!-- End Icon -->
+
+      <!-- Right Content -->
+      <div class="grow pb-8 group-last:pb-0">
+        <h3 class="mb-1 text-xs text-gray-600 dark:text-neutral-400">
+          2022 - Present
+        </h3>
+
+        <p class="font-semibold text-sm text-gray-800 dark:text-neutral-200">
+          Software Engineer level - II
+        </p>
+
+        <p class="font-medium text-sm text-gray-800 dark:text-neutral-200">
+          Jamaica Money Market Brokers
+        </p>
+
+        <p class="mt-1 text-sm text-gray-600 dark:text-neutral-400">
+          I am a Software Engineer level - II at a company called JMMB
+        </p>
+
+        <ul class="list-disc ms-6 mt-3 space-y-1.5">
+          <li class="ps-1 text-sm text-gray-600 dark:text-neutral-400">
+            Developed and implemented a POS system using C#, .NET and ASP.NET Core, resulting in a 15% increase in processing efficiency.
+          </li>
+          <li class="ps-1 text-sm text-gray-600 dark:text-neutral-400">
+            Optimized SQL queries to improve back-end server performance by 20%, reducing response times by an average of 2 seconds.
+          </li>
+          <li class="ps-1 text-sm text-gray-600 dark:text-neutral-400">
+            Implemented security enhancements for the POS system, including encryption protocols, that resulted in a 30% reduction in data breaches.
+          </li>
+        </ul>
+      </div>
+    <!-- End Right Content -->
+    </div>
+    <!-- End Item -->
+
+    <!-- Item -->
+    <div class="group relative flex gap-x-5">
+      <!-- Icon -->
+      <div class="relative group-last:after:hidden after:absolute after:top-8 after:bottom-2 after:start-3 after:w-px after:-translate-x-[0.5px] after:bg-gray-200 dark:after:bg-neutral-700">
+        <div class="relative z-10 size-6 flex justify-center items-center">
+          <svg
+            class="shrink-0 size-6 text-gray-800 dark:text-neutral-200"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ><path d="M12 12h.01" /><path d="M16 6V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v2" /><path d="M22 13a18.15 18.15 0 0 1-20 0" /><rect
+            width="20"
+            height="14"
+            x="2"
+            y="6"
+            rx="2"
+          /></svg>
+        </div>
+      </div>
+      <!-- End Icon -->
+
+      <!-- Right Content -->
+      <div class="grow pb-8 group-last:pb-0">
+        <h3 class="mb-1 text-xs text-gray-600 dark:text-neutral-400">
+          2021 - 2022
+        </h3>
+
+        <p class="font-semibold text-sm text-gray-800 dark:text-neutral-200">
+          Freelance Frontend Developer
+        </p>
+
+        <ul class="list-disc ms-6 mt-3 space-y-1.5">
+          <li class="ps-1 text-sm text-gray-600 dark:text-neutral-400">
+            Worked with clients to design and develop custom websites, resulting in a 20% increase in client satisfaction.
+          </li>
+          <li class="ps-1 text-sm text-gray-600 dark:text-neutral-400">
+            Developed and maintained strong client relationships through effective communication and project management.
+          </li>
+          <li class="ps-1 text-sm text-gray-600 dark:text-neutral-400">
+            Utilized tools such as Figma, Adobe XD, and Sketch to create wireframes and mockups for client approval.
+          </li>
+        </ul>
+      </div>
+    <!-- End Right Content -->
+    </div>
+  </div>
+  <!-- End Item -->
+</template>
+
+<style scoped></style>
