649cb374de1540f7ac9090c3868d95c10139694f
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-08-01 11:27:32 -0500

    refactor: Add Prose components for content styling

---
+++ app/components/prose/Content.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="prose prose-zinc dark:prose-invert max-w-none">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.prose :deep(:first-child) {
+  margin-top: 0;
+}
+.prose :deep(:last-child) {
+  margin-bottom: 0;
+}
+</style>
---
+++ app/components/prose/content/Body.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="max-w-5xl prose dark:prose-invert prose-zinc">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+div> :deep(*> :first-child) {
+  margin-top: 0;
+}
+</style>
