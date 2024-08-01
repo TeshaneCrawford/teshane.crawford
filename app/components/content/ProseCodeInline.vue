b83b0210b5f3a021f13a577ae8f293a9259d39ca
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-08-01 16:30:45 -0500

    feat: Add ProseCodeInline component for displaying inline code snippets in prose pages

---
+++ app/components/content/ProseCodeInline.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <code class="mx-1px px-[0.375rem] py-[0.2rem] rounded-md font-mono font-medium bg-cyan/30 dark:bg-cyan/40 before:content-[''] after:content-['']">
+    <slot />
+  </code>
+</template>
+
+<style scoped>
+</style>
