01a3a386d8bf31e0fcf39b76cdeb0179ec855bd9
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-07-31 22:09:52 -0500

    feat: Add font.css to main.css for custom font styling

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,6 +23,7 @@ export default defineNuxtConfig({
   // https://unocss.com/docs/nuxt
   css: [
     '@/assets/css/main.css',
+    '@/assets/css/font.css',
     '@unocss/reset/tailwind.css',
   ],
 
