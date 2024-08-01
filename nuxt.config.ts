8cbda9924914a2af496c40c11e6e579cf720594b
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-08-01 10:21:01 -0500

    refactor: Add preconnect links for Google Fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,6 +33,14 @@ export default defineNuxtConfig({
       meta: [{ charset: 'utf-8' }],
       link: [
         {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com',
+        },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+        },
+        {
           rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap',
         },
