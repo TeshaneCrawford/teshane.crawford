cf975adc7545a5d522d775ea61b8b8020232259f
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-08-01 10:58:09 -0500

    refactor: Add Grid component for responsive grid layout

---
+++ app/components/app/Grid.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-16 items-start">
+    <slot />
+  </div>
+</template>
+
+<style scoped></style>
