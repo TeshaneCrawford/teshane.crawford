9145fcfd05a2cc38fdec4fdb26eecc4ef07d68a1
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-08-01 09:07:02 -0500

    refactor: Update uno.config.ts with new shortcuts and font families

--- uno.config.ts
+++ uno.config.ts
@@ -11,6 +11,16 @@ import {
 
 export default defineConfig({
   shortcuts: [
+    ['linear-text', 'text-transparent bg-clip-text bg-gradient-to-r'],
+    ['text-p-r', 'linear-text from-cyan to-indigo'],
+    ['text-p-r-r', 'linear-text from-teal to-indigo'],
+
+    ['icon', 'w-4.5 h-4.5 cursor-pointer select-none transition-opacity-300 ease-in-out'],
+    ['icon-btn', 'icon color-inherit op64 hover-op100 hover-color-teal-500 dark-hover-color-inherit'],
+    ['icon-link', 'icon color-inherit op64 hover:op100 hover-text-red-300 dark-hover-color-inherit'],
+    ['icon-text', 'color-inherit op64 hover:op100 hover-text-purple dark-hover-color-inherit'],
+    ['linkInProse', 'trans c-context'],
+
     [/^badge-(.*)$/, ([, c]) => `bg-${c}4:10 text-${c}5 rounded`],
     [/^badge-xs-(.*)$/, ([, c]) => `badge-${c} text-xs px2 py0.5`],
     [/^badge-sm-(.*)$/, ([, c]) => `badge-${c} text-sm px3 py0.6`],
@@ -19,7 +29,12 @@ export default defineConfig({
   ],
   theme: {
     colors: {
-      // ...
+      context: 'rgba(var(--c-context),%alpha)',
+    },
+    fontFamily: {
+      dank: 'dank',
+      sans: 'DM Sans',
+      mono: 'DM Mono',
     },
   },
   presets: [
@@ -45,8 +60,8 @@ export default defineConfig({
     }),
     presetWebFonts({
       fonts: {
-        sans: 'DM Sans:400, 500, 600, 700',
-        mono: 'DM Mono',
+        // sans: 'DM Sans:400, 500, 600, 700',
+        // mono: 'DM Mono',
         // dank: 'Dank Mono, monospace',
       },
     }),
