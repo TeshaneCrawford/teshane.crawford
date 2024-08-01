8dbe86d9501f83f1e456c4ad7cc24b96925cae6c
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-08-01 10:53:20 -0500

    refactor: Add Main.vue component for blog page

---
+++ app/components/blog/Main.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="min-h-[calc(100vh-4rem)] py-4 md:py-4">
+    <div
+      v-if="$slots.header"
+      class="mb-8 lg:mb-16"
+    >
+      <slot name="header" />
+    </div>
+
+    <slot />
+  </main>
+</template>
+
+<style scoped></style>
