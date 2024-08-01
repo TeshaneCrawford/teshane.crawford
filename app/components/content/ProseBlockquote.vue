6f1bd5661b6a89aa1f0458d20215b96eea9fc161
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-08-01 16:30:30 -0500

    feat: Add ProseBlockquote component for displaying blockquotes in prose pages

---
+++ app/components/content/ProseBlockquote.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <blockquote class="mt-6 border-l-2 pl-6 italic font-mono text-sm">
+    <slot />
+  </blockquote>
+</template>
+
+<style scoped></style>
