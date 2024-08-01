f42eb143e010375fc652c3cc838dde2989b4fb5a
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-07-31 23:52:37 -0500

    refactor: Update abouts.vue component with new sections and styling

--- app/pages/abouts.vue
+++ app/pages/abouts.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    Page: abouts
+  <div class="relative z-20 w-full max-w-4xl mx-auto mt-16 px-7 md:mt-24 lg:mt-2 xl:px-0">
+    <AboutIntro />
+    <HomeSeparator text="Timeline of my journey" />
+    <AboutTimeline />
+    <HomeSeparator text="My core skillset" />
+    <AboutSkills />
   </div>
 </template>
 
