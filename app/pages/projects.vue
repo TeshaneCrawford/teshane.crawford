99e5f4d60fb7a9768d971a9eea9e111ff6754b10
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-08-02 11:06:16 -0500

    feat: Add projects page with SEO meta data

---
+++ app/pages/projects.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Projects - Teshane Crawford',
+  description: 'Projects I\'ve created or contributed to.',
+})
+</script>
+
+<template>
+  <ProjectSection />
+</template>
+
+<style scoped></style>
