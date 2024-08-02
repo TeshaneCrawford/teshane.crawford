c36fd63d840a355b1b0b43ecd4af7498c90472b5
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-08-02 11:06:09 -0500

    feat: Add ChatSection component for displaying chat section

---
+++ app/pages/chat.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Chat - Teshane Crawford',
+  description: 'Chat with me. I\'m available via these platforms.',
+})
+</script>
+
+<template>
+  <ChatSection />
+</template>
+
+<style scoped></style>
