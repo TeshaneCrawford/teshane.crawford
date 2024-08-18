b4b3a80c4a201802d1c3bedb9ab1658da65a77fe
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-08-17 22:50:25 -0500

    refactor: Update Footer.vue to include social icons and copyright information

--- app/components/app/Footer.vue
+++ app/components/app/Footer.vue
@@ -2,18 +2,41 @@
 
 <template>
   <footer class="slide-enter py-2 prose animate-delay-1200!">
-    <div
-      class="mb"
-    >
-      <AppSocialIcons />
-    </div>
-    <div class="mb-2">
-      <span class="text-sm op50"><NuxtLink
-        target="_blank"
-        to="https://creativecommons.org/licenses/by-nc-sa/4.0/"
-        style="color:inherit"
-      >CC BY-NC-SA 4.0</NuxtLink> 2021 - {{ new Date().getFullYear() }} © Teshane Crawford</span>
-      <div class="flex-auto" />
+    <div class="space-y-4">
+      <div
+        class="flex flex-col md:flex-row md:items-center"
+      >
+        <h4 class="font-light">
+          Find me on:
+        </h4>
+        <div class="flex flex-wrap">
+          <AppSocialIcons />
+        </div>
+      </div>
+      <!-- <div
+        class="flex flex-col md:flex-row gap-2 md:items-center"
+      >
+        <h4 class="font-light">
+          You can email me at:
+        </h4>
+        <div class="flex">
+          <NuxtLink
+            target="_blank"
+            to="mailto:@crawfordteshane@gmail.com"
+            label="crawfordteshane@gmail.com"
+          >
+            crawfordteshane@gmail.com
+          </NuxtLink>
+        </div>
+      </div> -->
+      <div class="">
+        <span class="text-sm op50"><NuxtLink
+          target="_blank"
+          to="https://creativecommons.org/licenses/by-nc-sa/4.0/"
+          style="color:inherit"
+        >CC BY-NC-SA 4.0</NuxtLink> 2021 - {{ new Date().getFullYear() }} © Teshane Crawford</span>
+        <div class="flex-auto" />
+      </div>
     </div>
   </footer>
 </template>
