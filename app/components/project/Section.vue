570216859c473438bfd020f7a6854ca860f78091
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-08-27 20:40:16 -0500

    Add lazy loading to projects

--- app/components/project/Section.vue
+++ app/components/project/Section.vue
@@ -1,38 +1,6 @@
 <script setup lang="ts">
-// import type { Repo } from '../../../types/project'
-const { data: repoGroup } = await useFetch('/api/repos')
+const { data: repoGroup } = await useLazyFetch('/api/repos')
 const { data: doc } = await useLazyAsyncData('projects', () => queryContent('/projects/').findOne())
-
-// onMounted(async () => {
-//   const data = await $fetch<Repo[]>('https://api.github.com/users/TeshaneCrawford/repos?sort=updated')
-//   const publicRepos = data.filter(repo => !repo.private && !repo.archived)
-//   const publicAndNotForkRepos = publicRepos.filter(repo => !repo.fork)
-
-//   const repoGroups: Record<string, Repo[]> = {
-//     'Angular': filterRepos(publicAndNotForkRepos, 'angular'),
-//     'Vue Ecosystem': filterRepos(publicAndNotForkRepos, 'vue'),
-//     'React': filterRepos(publicAndNotForkRepos, 'react'),
-//     'C#': filterRepos(publicAndNotForkRepos, 'csharp'),
-//     'TypeScript': filterRepos(publicAndNotForkRepos, 'typescript'),
-//     'JavaScript': filterRepos(publicAndNotForkRepos, 'javascript'),
-//     'Python': filterRepos(publicAndNotForkRepos, 'python'),
-//     'Apps': filterRepos(publicAndNotForkRepos, 'apps'),
-//     'Mobile': filterRepos(publicRepos, 'mobile'),
-//     'UI': filterRepos(publicRepos, 'ui'),
-//     'API': filterRepos(publicAndNotForkRepos, 'api'),
-//     'Library': filterRepos(publicAndNotForkRepos, 'library'),
-//     'Templates': filterRepos(publicAndNotForkRepos, 'template'),
-//     'All': publicAndNotForkRepos,
-//   }
-
-//   repoGroup.value = repoGroups
-// })
-
-// function filterRepos(repos: Repo[], key: string) {
-//   return repos.filter(repo => repo.topics && repo.topics.includes(key))
-// }
-
-// const repoGroup = ref({} as Record<string, Repo[]>)
 </script>
 
 <template>
