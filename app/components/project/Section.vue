815527e5e33b2349551829581a4905df2de2a4ee
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-09-13 14:24:03 -0500

    revert

--- app/components/project/Section.vue
+++ app/components/project/Section.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-const { data: repoGroup } = await useAsyncData('repos', () => $fetch('/api/repos'))
-const { data: doc } = await useAsyncData('projects', () => queryContent('/projects/').findOne())
+const { data: repoGroup } = await useLazyFetch('/api/repos')
+const { data: doc } = await useLazyAsyncData('projects', () => queryContent('/projects/').findOne())
 </script>
 
 <template>
@@ -8,17 +8,12 @@ const { data: doc } = await useAsyncData('projects', () => queryContent('/projec
     :title="doc?.title"
     :description="doc?.description"
   >
-    <Suspense>
-      <ProjectGitHubRepoPanel
-        v-for="(repos, key) in repoGroup"
-        :key="key"
-        :label="key"
-        :data="repos"
-      />
-      <template #fallback>
-        <div>Loading repositories...</div>
-      </template>
-    </Suspense>
+    <ProjectGitHubRepoPanel
+      v-for="(repos, key) in repoGroup"
+      :key="key"
+      :label="key"
+      :data="repos"
+    />
   </AppPageHeading>
 </template>
 
