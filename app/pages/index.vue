a70d107bf4174c3d36911f06498528d1ce0801f3
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-10-06 17:44:56 -0500

    update home page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -18,9 +18,7 @@ useHead({
 
 <template>
   <div>
-    <div
-      class="relative z-20 mx-auto mt-16 max-w-4xl w-full lg:mt-12 md:mt-24 md:px-7 xl:px-0"
-    >
+    <div class="relative z-20 mx-auto mt-16 max-w-4xl w-full lg:mt-12 md:mt-24 md:px-7 xl:px-0">
       <div class="flex flex-col items-center md:flex-row">
         <div class="relative w-full">
           <!-- <h1
@@ -38,111 +36,72 @@ useHead({
             </span>
           </h1>
 
-          <p
-            mt-2
-            font-dank
-            italic
-          >
+          <p mt-2 font-dank italic>
             「 Whatever is worth doing, do it well. 」
           </p>
 
           <p class="mb-6 mt-6 text-base text-neutral-600 dark:text-neutral-400">
-            I'm a <code
+            I'm a <code text-p-r-r-r text-xl font-semibold font-dank v-text="`<Software Engineer />`" />. <br
+              class="hidden lg:block">Building Accessible Web and Mobile Apps.
+          </p>
 
-              text-p-r-r-r
-              text-xl
-              font-semibold
-              font-dank
-              v-text="`<Software Engineer />`"
-            />. <br
-              class="hidden lg:block"
-            >Building Accessible Web and Mobile Apps.
-          </p>
-          <p class="mb-2 text-neutral-800 font-semibold dark:text-neutral-200">
-            I can help you out with:
-          </p>
-          <ul
-            class="list-disc list-inside py-2 text-sm text-neutral-500 space-y-[3px] dark:text-neutral-400"
-          >
-            <li>Vue.js Development</li>
-            <li>React.js Development</li>
-            <li>Node.js Development</li>
-            <li>.NET Development</li>
-            <li>and more...</li>
-          </ul>
-          <p class="mt text-base text-neutral-600 dark:text-neutral-400">
-            Let's collaborate on innovative projects! Contact me to discuss your needs.
-          </p>
-          <div class="mt-2 list-none">
-            <NuxtLink
-              to="mailto:crawfordteshane@gmail.com"
-              class="flex items-center"
-            >
-              <span
-                class="social-home-button"
-              >
-                <icon
-                  name="uil-message"
-                  class="w-4 flex-inline"
-                />
-              </span>
-              <span
+          <div class="">
 
-                ml-4
-                underline
-                underline-offset-6
-                decoration-wavy
-                op-90
-              >crawfordteshane@gmail.com</span>
-            </NuxtLink>
+            <p mb>
+              Hey, I'm <strong>Teshane Crawford</strong>, a software engineer & an open source enthusiast.
+            </p>
+            <p mb>
+              I am passionate about building software that solves real-world problems. I am a big admirer of the
+              <i i-logos-vitejs mr-1 h-1.1em w-1.1em />
+              <a linear-text shape-bottom-right trans class="c-context::#bd34fe" from="#41d1ff" to="#bd34fe"
+                href="https://github.com/vitejs/vite" target="_blank">Vite</a>、
+              <i i-logos-vue mr-1 h-1.1em w-1.1em />
+              <a linkInProse o="#64b687" href="https://github.com/vuejs-translations/docs-zh-cn"
+                target="_blank">Vue</a>、
+              <i i-logos-nuxt-icon mr-1 />
+              <a linkInProse o="#64d98a" href="https://github.com/nuxt/framework" target="_blank">Nuxt</a> community.
+              <i i-fluent-emoji-partying-face />
+            </p>
+            <p mb>
+              I have a wide range of interests, coding, watching wildlife documentaries, listening to music, learning
+              foreign
+              languages, playing games... Recently, I have been working on the backend, using
+              <i i-logos-java mr-1 />
+              <a linkInProse o="#f46d01" href="" target="_blank">Java</a>
+              <span mr-1 />
+              and <i i-logos-spring-icon mr-1 />
+              <a linkInProse o="#6bd33f" href="" target="_blank">SpringBoot</a>.
+            </p>
+            <p mb>
+              I use quite a number of technologies, but I am most comfortable with
+              <i i-logos-react mr-1 h-1.1em w-1.1em />
+              <a linkInProse o="#61dafb" href="https://react.dev/" target="_blank"><strong>React.js</strong></a>,
+              <span mr-1 />
+              <i i-logos-vue mr-1 h-1.1em w-1.1em />
+              <a linkInProse o="#64b687" href="https://vuejs.org/" target="_blank"><strong>Vue.js</strong></a>,
+              <span mr-1 />
+              <!-- <i i-my-logos-angular-icon mr-1 />
+      <a trans linear-text shape-bl class="c-context::#c3002f" from="#9717E7" via="#F20755" to="#FF31D9"
+        href="https://angular.dev/" target="_blank"><strong> Angular</strong></a>, -->
+              <icon name="i-logos-angular-icon" mr-1 />
+              <a linear-text shape-bl trans class="c-context::#c3002f" from="#9717E7" via="#F20755" to="#FF31D9"
+                href="https://angular.dev/" target="_blank"><strong> Angular</strong></a>,
+              <span mr-1 />
+              and <i i-logos-dotnet mr-1 />
+              <a linkInProse o="#512bd4" href="https://dotnet.microsoft.com/" target="_blank"><strong>.NET</strong></a>.
+              If you're interested in
+              knowing what other technologies I use, check out my <NuxtLink to="/tools" text-p-r-r trans>
+                <strong>Tools</strong>
+              </NuxtLink> page.
+            </p>
+            <p>
+              <a class="group" text-p-r trans hover-op-75>
+                Hire Me
+                <i group-hover="ml-1" i-ri:arrow-right-up-line bg-gradient-to-r trans />
+              </a>
+            </p>
           </div>
-          <AppButton
-            text="Get in contact - Hire me"
-            link="/chat"
-          />
-          <NuxtLink
-            class="ml-4 border border-rounded bg-white/60 p-1.5 dark:bg-white/10 hover:bg-gray-50 dark:hover:bg-white/5"
-            target="_blank"
-            to="https://www.linkedin.com/in/teshanecrawford/"
-            title="Linkedin"
-          >
-            <icon
-              name="uil-linkedin-alt"
-              class="w-6"
-            /> <span
-              text-sm
-              font-semibold
-            >LinkendIn</span>
-          </NuxtLink>
         </div>
-        <!-- <div
-          class="relative justify-end hidden w-full mt-10 md:flex md:pl-10 md:w-1/2 md:mt-0 md:translate-y-4 xl:translate-y-0"
-        >
-          <div class="relative z-50 w-full">
-            <div
-              class="absolute bottom-0 z-40 w-16 h-16 -translate-x-6 -translate-y-1/2 lg:top-auto top-0 lg:-translate-y-[330px] rounded-full"
-            >
-              <span
-                class="relative z-20 flex items-center justify-center w-full h-full text-2xl border-8 border-white rounded-full dark:border-neutral-950 bg-neutral-100 dark:bg-neutral-900"
-              >
-                <span
-                  class="flex items-center justify-center w-full h-full bg-white border border-dashed rounded-full dark:bg-neutral-950 border-neutral-300 dark:border-neutral-700"
-                >👋</span>
-              </span>
-            </div>
-            <div class="relative z-30 px-10">
-              <img
-                src="/images/photo.png"
-                loading="eager"
-                decoding="auto"
-                class="relative z-30 w-full aspect-[790/1189] md:max-w-md mx-auto dark:-translate-y-0.5"
-              >
-            </div>
-            <div
-              class="absolute bottom-0 right-0 z-20 w-full h-full lg:h-[420px] translate-x-0 -translate-y-px border border-dashed rounded-2xl bg-gradient-to-r dark:from-neutral-950/20 dark:via-black dark:to-neutral-950/20 from-white via-neutral-50 to-white border-neutral-300 dark:border-neutral-700"
-            />
-          </div>
-        </div> -->
       </div>
     </div>
     <HomeSeparator text="Some of My Projects" />
@@ -153,7 +112,7 @@ useHead({
 </template>
 
 <style scoped>
-  .social-home-button {
-    @apply mr-[6px] h-[36px] flex justify-center items-center bg-[#E8E8E8]/10 w-[36px] border rounded-full dark:bg-[#232425] hover:bg-gray-50 dark:hover:bg-white/5;
-  }
+.social-home-button {
+  @apply mr-[6px] h-[36px] flex justify-center items-center bg-[#E8E8E8]/10 w-[36px] border rounded-full dark:bg-[#232425] hover:bg-gray-50 dark:hover:bg-white/5;
+}
 </style>
